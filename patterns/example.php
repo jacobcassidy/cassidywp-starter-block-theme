af3f58520ac24ccc385a0cad9de2f4db647f9c14
Jacob Cassidy <jacob@jacobcassidy.com>
2024-09-24 16:42:35 +0700

    Add example block pattern file

---
+++ patterns/example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Example Pattern
+ * Slug: cassidywp-starter-block-theme/example-pattern
+ * Categories: page
+ * Keywords: example, pattern
+ * Block Types: core/post-content
+ * Viewport Width: 1440
+ *
+ * @package CassidyWP\StarterBlockTheme\Patterns
+ * @version 1.0.0
+ */
+
+?>
+
+<!-- Block code here. -->
