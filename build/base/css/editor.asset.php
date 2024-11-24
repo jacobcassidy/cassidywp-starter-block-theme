f55656ca70721b24c5d90a3869c31e72f19e184c
Jacob Cassidy <jacob@jacobcassidy.com>
2024-11-24 18:25:04 +0700

    Add build files

---
+++ build/base/css/editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'c3ba38b7d1764c836d28');
---
+++ build/base/js/script.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ece37c3eab58c6d6e611');
